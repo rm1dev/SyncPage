bc6d9e9de135551490309ec941b4a8639a218911
rm1dev <r324.moghaddam@gmail.com>
2026-08-08 02:31:41 +0330

    feat(admin): Add SyncModule to admin dependencies
    - Import SyncModule from sync module
    - Register SyncModule in AdminModule imports array
    - Enable sync functionality access within admin context

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -3,10 +3,11 @@ import { FormEngineModule } from '../form-engine/form-engine.module';
 import { DeploymentModule } from '../deployment/deployment.module';
 import { NodesModule } from '../nodes/nodes.module';
 import { UpdatesModule } from '../updates/updates.module';
+import { SyncModule } from '../sync/sync.module';
 import { AdminController } from './admin.controller';
 
 @Module({
-  imports: [FormEngineModule, DeploymentModule, NodesModule, UpdatesModule],
+  imports: [FormEngineModule, DeploymentModule, NodesModule, UpdatesModule, SyncModule],
   controllers: [AdminController],
 })
 export class AdminModule {}
